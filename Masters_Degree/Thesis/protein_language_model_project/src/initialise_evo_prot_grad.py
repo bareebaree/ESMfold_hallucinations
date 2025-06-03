1a7605b61dcced2f6bbaf2ad801b90ddd34513c7
bareebaree <james_tate@ymail.com>
2025-06-03 17:38:46 +0100

    Update initialise_evo_prot_grad.py
    commented functions

--- Masters_Degree/Thesis/protein_language_model_project/src/initialise_evo_prot_grad.py
+++ Masters_Degree/Thesis/protein_language_model_project/src/initialise_evo_prot_grad.py
@@ -8,16 +8,25 @@ import glob
 from collections import deque
 
 # === CONFIGURATION ===
+"""
+Set initial directories to be used. Script will utilise the data_dir as an input, and output to results_dir. If using running multiple loops in parallel, it is prudent to 
+set different results directories. Additionally, alter your protein folder name, this is called "dehydrogenase/dehydrogenase_fastas" in this.
 initial_fastas_dir = './data/initial_proteins/dehydrogenase/dehydrogenase_fastas/most_distant_sequences'
 data_dir = './data/iterations/'
 results_dir = './results/'
 os.makedirs(data_dir, exist_ok=True)
 os.makedirs(results_dir, exist_ok=True)
-
+"""
 # === INITIALISE EXPERT ===
-ems_expert = EsmExpertModified(scoring_strategy='pseudolikelihood_ratio', temperature=1.0)
+# Define and instantiate your expert from the experts in the ./experts folder. For the purposes of this project, only the ESM2 expert was used, 
+# as the intention was to optimise and focus on a single model's predictions. To utilise CPU instead, change device to 'cpu'.
+ems_expert = EsmExpertModified(scoring_strategy='pseudolikelihood_ratio', temperature=1.0, device = 'cuda')
 
 def get_latest_iteration_log(iteration_number: int) -> str:
+    """
+    Obtains filepath for logfile for any given iteration of the MCMC loop. 
+    This is used to extract the filepath for the previous iteration, so the highest scoring sequence can be extracted from it.
+    """
     pattern = os.path.join(results_dir, f"evoprotgrad_run_log_iter{iteration_number}_*.csv")
     matches = glob.glob(pattern)
     if not matches:
@@ -26,6 +35,11 @@ def get_latest_iteration_log(iteration_number: int) -> str:
     return latest
 
 def extract_best_variant_to_fasta(log_csv_path: str, output_fasta_path: str, iteration: int):
+    """
+    From the extracted sequence from get_latest_iteration_log(), extract the sequence paired with the highest score from this run
+    and write to fasta, using the iteration, chain, and score as the header for the fasta.
+    """
+    
     df = pd.read_csv(log_csv_path)
     if df.empty:
         raise ValueError(f"Log {log_csv_path} is empty.")
@@ -42,10 +56,13 @@ def extract_best_variant_to_fasta(log_csv_path: str, output_fasta_path: str, ite
 fasta_files = glob.glob(os.path.join(initial_fastas_dir, '*.fasta'))
 
 for initial_fasta in fasta_files:
+    # Loop to call the DirectedEvolution class on each fasta in the target fasta file directory. pdb_id is sliced off at the :4 position and
+    # extracted for logging.
     pdb_id = os.path.basename(initial_fasta)[:4]
     print(f"\n🚀 Starting evolution for {pdb_id}...")
 
     # === INITIAL ITERATION ===
+    # Initialisation MCMC run.
     evo_init = evo_prot_grad.DirectedEvolution(
         wt_fasta=initial_fasta,
         output='all',
@@ -63,11 +80,15 @@ for initial_fasta in fasta_files:
     init_fasta = os.path.join(data_dir, f'{pdb_id}_iteration_0.fasta')
     extract_best_variant_to_fasta(init_log_path, init_fasta, iteration=0)
 
+    # initialise logging for iterations
     iteration = 1
+
+    #define number of scores to take average of last n for. This is used to allow the loop to end when the model converges.
     recent_best_scores = deque(maxlen=10)
     wt_fasta = init_fasta
 
     # === EVOLUTION LOOP ===
+    # DirectedEvolution() call for the main MCMC loop.
     while True:
         print(f"\n🧬 Running evo iteration {iteration} for {pdb_id}...")
 
@@ -92,6 +113,8 @@ for initial_fasta in fasta_files:
         best_score = df['score'].max()
         recent_best_scores.append(best_score)
 
+        # This block of code ensures the model stops at close to convergance (average best PoE score of last ten runs is ≤ 0.01.
+        # 0.01 is used to approximate convergance as run will take far too long to reach 0, if it can at all. 
         if len(recent_best_scores) == 10:
             avg_score = sum(recent_best_scores) / 10
             print(f"📊 Average best score over last 10 iterations: {avg_score:.4f}")
